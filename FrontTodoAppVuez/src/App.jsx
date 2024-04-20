78d307fbe230cd196953b32f391ba8bb81fd36b2
deividcool <velozadeivid@gmail.com>
2024-04-20 02:11:43 -0500

    Home
    -create tittle
    -cards

--- FrontTodoAppVuez/src/App.jsx
+++ FrontTodoAppVuez/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import {Header} from './components/Header'
+import { PrincipalCard } from './components/PrincipalCard'
 
 function App() {
 
   return (
-    <main className='flex flex-1 h-80 w-80 bg-slate-50'>
+    <main className='flex flex-1 h-screen w-full bg-[#0E0F12] flex-col xl:w-[1280px]'>
+      <Header />
+      <div className='flex flex-row flex-1 p-4 justify-evenly'>
+        <PrincipalCard Tittle={"TO-DO"} />
+        <PrincipalCard Tittle={"IN-PROOGRESS"} />
+        <PrincipalCard Tittle={"COMPLETED"} />
+      </div>
      
     </main>
   )
